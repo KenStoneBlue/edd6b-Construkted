34063268faa6e245869d5b9bc6b7c04db25ca790
worldweb <info@worldwebtechnology.com>
2019-05-03 11:58:35 +0530

    Add Ola Child Theme
    - Override single download page template in child theme.

---
+++ olam-child/functions.php
@@ -0,0 +1,12 @@
+<?php
+function olam_enqueue_styles() {
+
+	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+}
+
+function olam_enqueue_custom_styles() {
+
+	wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( 'parent-style' ) );
+}
+add_action( 'wp_enqueue_scripts', 'olam_enqueue_styles');
+add_action( 'wp_enqueue_scripts', 'olam_enqueue_custom_styles', 18);
