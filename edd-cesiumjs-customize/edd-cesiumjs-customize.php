08a82bf614c824f8ec429f0791b213ba26d0c5f1
Adrian <aonsen@gmail.com>
2019-07-13 11:13:30 -0400

    Update edd-cesiumjs-customize.php

--- edd-cesiumjs-customize/edd-cesiumjs-customize.php
+++ edd-cesiumjs-customize/edd-cesiumjs-customize.php
@@ -33,9 +33,9 @@
  * - Added a hyphen between slug and file name when file gets renamed before sending to S3
  * - Added load speed optimizations as suggested by Omar
  * - Modified keyboard key assignments, so "A" and "D" move the camera rather then rotate it.
- 1.0.5
-- Remove the CDATA tags that appear in the html source for the asset pages
-- Background terrain is not showing up anymore
+ * 1.0.5
+ * - Remove the CDATA tags that appear in the html source for the asset pages
+ * - Background terrain is not showing up anymore
  
  */
 
